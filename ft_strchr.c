c2195d05c69bcb8d08f1bcbbdfe96424b9587f34
chanma <yunosuke.homma@icloud.com>
2022-05-29 17:12:37 +0900

    add strchr

---
+++ ft_strchr.c
@@ -0,0 +1,39 @@
+char	*ft_strchr(const char *s, int c)
+{
+	char	*new_s;
+	int	i;
+
+	new_s = (char *) s;
+	i = 0;
+	while (new_s[i] != '\0')
+	{
+		if (new_s[i] == (char) c)
+		{
+			return (new_s + i);
+		}
+		i++;
+	}
+	if ((char) c == '\0')
+	{
+		return (new_s + i);
+	}
+	return (0);
+}
+
+/*
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	char a[] = "abcdefg";
+	int b = '\0';
+
+	printf("%s\n", ft_strchr(a, b));
+
+	char c[] = "abcdefg";
+	int d = '\0';
+
+	printf("%s\n", strchr(c, d));
+}
+*/
