4c15787c9e84145cf750a6366a6674159f0a2997
chanma <yunosuke.homma@icloud.com>
2022-05-29 17:14:05 +0900

    add tolower

---
+++ ft_tolower.c
@@ -0,0 +1,19 @@
+int	ft_tolower(int	c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		c += 32;
+	}
+	return (c);
+}
+
+/*
+#include <stdio.h>
+
+int main(void)
+{
+	int a = '8';
+
+	printf("%c", ft_tolower(a));
+}
+*/
