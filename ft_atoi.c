f719e6ed32b5b9ef800e6d72e9ef7071f1c53895
chanma <yunosuke.homma@icloud.com>
2022-06-02 12:46:38 +0900

    add atoi

---
+++ ft_atoi.c
@@ -0,0 +1,40 @@
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	int	new_s;
+	int	sign;
+	size_t	i;
+
+	new_s = 0;
+	sign = 1;
+	i = 0;
+	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\r'||
+			str[i] == '\f' || str[i] == '\t' || str[i] == '\v')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= 48 && str[i] <= 57)
+	{
+		new_s = (new_s * 10) + (str[i] - 48);
+		i++;
+	}
+	return (new_s * sign);
+}
+
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+int	main(void)
+{
+	char a[] = "	 \n-42 3";
+
+	printf("%d\n", atoi(a));
+	printf("%d\n", ft_atoi(a));
+}
+*/
