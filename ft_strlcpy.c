eb0aaccd18e394938bdd038cc03c05fdd03611f5
chanma <yunosuke.homma@icloud.com>
2022-06-03 14:36:40 +0900

    change strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,17 +1,5 @@
 #include "libft.h"
 
-size_t	ft_strlen(const char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-
 size_t	ft_strlcpy(char *dest, const char *src, size_t n)
 {
 	size_t	i;
