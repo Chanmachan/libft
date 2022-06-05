72a460e9d0fc11b21b37a80288786f40018bf99d
chanma <yunosuke.homma@icloud.com>
2022-06-05 14:11:28 +0900

    add memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,14 +1,14 @@
 #include "libft.h"
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	size_t	i;
 
 	i = 0;
-	while (i < n && ((unsigned char *)src)[i] != '\0')
+	while (i < n)
 	{
-		((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
 		i++;
 	}
-	return (dest);
+	return (dst);
 }
