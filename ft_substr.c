a8413a651148ad59b8f1fc1a13666a12a6810d20
chanma <yunosuke.homma@icloud.com>
2022-06-05 17:26:46 +0900

    change substr

--- ft_substr.c
+++ ft_substr.c
@@ -5,41 +5,31 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	i;
 	char *new_s;
 	char *rtn_str;
-	unsigned int	len_s;
 
 	i = 0;
 	new_s = (char *) s;
-	len_s = ft_strlen(s);
 	rtn_str = (char *) malloc (sizeof(char) * (len + 1));
 	if (rtn_str == NULL)
-		return (0);
-	if (start < len_s)
+		return (NULL);
+	while (i < len)
 	{
-		while (--start)
-			new_s++;
-		while (len-- && new_s[i] != '\0')
-		{
-			rtn_str[i] = new_s[i];
-			i++;
-		}
-		rtn_str[i] = '\0';
-		return (rtn_str);
+		rtn_str[i++] = new_s[start++];
 	}
-	return (NULL);
+	rtn_str[i] = '\0';
+	return (rtn_str);
 }
 
-//L13 (len + 1) bracket needed
-//L20 new_s[i] != '\0' important
+//L16 new_s[start] != '\0' important
 
 /*
 #include <stdio.h>
 
 int main(void)
 {
-	char a[] = "     abc defg hi";
-	unsigned int b = 5;
-	size_t c = 1000;
+	char a[] = "tripouille";
+	unsigned int b = 100;
+	size_t c = 1;
 
-	printf("%s", ft_substr(a, b, c));
+	printf("%s\n", ft_substr(a, b, c));
 }
 */
