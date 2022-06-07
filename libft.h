aeeb21897bbd1266c65cf5ac3f05f9f301cd11b8
chanma <yunosuke.homma@icloud.com>
2022-06-07 17:13:45 +0900

    change libft.h

--- libft.h
+++ libft.h
@@ -15,7 +15,6 @@ int	ft_isalpha(int c);
 int	ft_isascii(int c);
 int	ft_isdigit(int c);
 int	ft_isprint(int c);
-char	*ft_itoa(int n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int	ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
@@ -36,5 +35,12 @@ int	ft_tolower(int	c);
 int	ft_toupper(int	c);
 char	*ft_strdup(char *str);
 void	*ft_calloc(size_t count, size_t size);
+char	*ft_itoa(int n);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
 #endif
