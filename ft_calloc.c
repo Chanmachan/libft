458bd99c623c5e8aaab8fcdee4e7f5c64086f43d
chanma <yunosuke.homma@icloud.com>
2022-07-01 21:32:44 +0900

    adjust **_MAX

--- ft_calloc.c
+++ ft_calloc.c
@@ -3,6 +3,26 @@
 void	*ft_calloc(size_t n, size_t size)
 {
 	void	*rtn_box;
+
+	if (n <= ULLONG_MAX && size <= ULLONG_MAX)
+	{
+		if (n >= SIZE_MAX && size >= SIZE_MAX)
+			return (NULL);
+		if (n == 0 || size == 0)
+			return (NULL);
+		rtn_box = (void *) malloc(n * size);
+		if (rtn_box == NULL)
+			return (NULL);
+		ft_memset(rtn_box, 0, n * size);
+		return (rtn_box);
+	}
+	return (NULL);
+}
+
+/*
+void	*ft_calloc(size_t n, size_t size)
+{
+	void	*rtn_box;
 	if(n <= 4294967295 && size <= 42949667295)
 	{
 		rtn_box = (void *) malloc (n * size);
@@ -12,6 +32,5 @@ void	*ft_calloc(size_t n, size_t size)
 		return (rtn_box);
 	}
 	return (NULL);
-}
-
-//L3 if n or size is max of size_t
+}*/
+//proto
