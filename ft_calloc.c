c37cd66a82fb9a1c3a4a1ef23841e09880a14f8f
chanma <yunosuke.homma@icloud.com>
2022-07-06 15:54:44 +0900

    delete invalid process

--- ft_calloc.c
+++ ft_calloc.c
@@ -4,19 +4,15 @@ void	*ft_calloc(size_t n, size_t size)
 {
 	void	*rtn_box;
 
-	if (n <= ULLONG_MAX && size <= ULLONG_MAX)
-	{
-		if (n >= SIZE_MAX && size >= SIZE_MAX)
-			return (NULL);
-		if (n == 0 || size == 0)
-			return (NULL);
-		rtn_box = (void *) malloc(n * size);
-		if (rtn_box == NULL)
-			return (NULL);
-		ft_memset(rtn_box, 0, n * size);
-		return (rtn_box);
-	}
-	return (NULL);
+	if (n >= SIZE_MAX && size >= SIZE_MAX)
+		return (NULL);
+	if (n == 0 || size == 0)
+		return (NULL);
+	rtn_box = (void *) malloc(n * size);
+	if (rtn_box == NULL)
+		return (NULL);
+	ft_memset(rtn_box, 0, n * size);
+	return (rtn_box);
 }
 
 /*
