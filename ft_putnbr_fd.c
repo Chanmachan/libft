978c7968f65c5f3e87bf8dccbdc5f45bccd48450
chanma <yunosuke.homma@icloud.com>
2022-07-06 16:38:14 +0900

    delete malloc

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -5,14 +5,26 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*itoa_str;
 
-	itoa_str = ft_itoa(n);
-	ft_putstr_fd(itoa_str, fd);
-	free (itoa_str);
+	if (n == -2147483648)
+	{
+		ft_putchar_fd('-', fd);
+		ft_putchar_fd('2', fd);
+		n = 147483648;
+	}
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		n *= -1;
+	}
+	if (n >= 10)
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
+	}
+	else
+		ft_putchar_fd(n + 48, fd);
 }
-
-//free needed
 
 /*
 int main(void)
