8848ab7841cd6bad24829028d85f05486b298ea9
chanma <yunosuke.homma@icloud.com>
2022-07-07 15:25:03 +0900

    adjust norm

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -4,11 +4,10 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	unsigned char	*new_s1;
 	unsigned char	*new_s2;
-	size_t	i;
+	size_t			i;
 
 	new_s1 = (unsigned char *) s1;
 	new_s2 = (unsigned char *) s2;
-
 	i = 0;
 	while (i < n)
 	{
