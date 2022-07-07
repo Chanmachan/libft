0329f1ee9008cc027f68705ca1144335891fa7eb
chanma <yunosuke.homma@icloud.com>
2022-07-07 17:45:37 +0900

    change all

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,22 +1,41 @@
 #include "libft.h"
+#include <stdio.h>
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	size_t	i;
-	unsigned char	tmp[n + 1];
+//	size_t			i;
+	unsigned char	*new_dest;
+	unsigned char	*new_src;
 
-	i = 0;
-	while (i < n)
+	new_dest = (unsigned char *)dest;
+	new_src = (unsigned char *)src;
+	if (n == 0)
+		return (dest);
+	if (dest == NULL || src == NULL)
+		return (0);
+	if (dest > src)
 	{
-		tmp[i] = ((unsigned char *)src)[i];
-		i++;
+		while (n > 0)
+		{
+			new_dest[n - 1] = new_src[n - 1];
+			n--;
+		}
 	}
-	tmp[i] = '\0';
-	i = 0;
-	while (i < n)
+	else
 	{
-		((unsigned char *)dest)[i] = tmp[i];
-		i++;
+		ft_memcpy(new_dest, new_src, n);
 	}
 	return (dest);
 }
+
+/*
+#include <stdio.h>
+
+int main()
+{
+	char	str[] = {67, 68, 67, 68, 69, 0, 45};;
+
+	ft_memmove(str + 1, str, 2);
+	printf("%s\n", str);
+}
+*/
