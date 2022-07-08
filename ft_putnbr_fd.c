a54384c1a413480ce695869cc64277e5473d3bb1
chanma <yunosuke.homma@icloud.com>
2022-07-09 00:29:05 +0900

    repair

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -15,7 +15,11 @@
 void	ft_putnbr_fd(int n, int fd)
 {
 	if (n == -2147483648)
-		ft_putstr_fd("-2147483648", 1);
+	{
+		ft_putchar_fd('-', fd);
+		ft_putchar_fd('2', fd);
+		n = 147483648;
+	}
 	if (n < 0)
 	{
 		ft_putchar_fd('-', fd);
@@ -30,9 +34,7 @@ void	ft_putnbr_fd(int n, int fd)
 		ft_putchar_fd(n + 48, fd);
 }
 
-/*
-int main(void)
+/*int main(void)
 {
 	ft_putnbr_fd(2147483647, 1);
-}
- */
+}*/
