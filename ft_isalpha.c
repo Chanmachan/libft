6c82962005b3c301d25b88da592d1393cf9332ad
chanma <yunosuke.homma@icloud.com>
2022-07-09 21:16:12 +0900

    65 -> A

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -14,14 +14,9 @@
 
 int	ft_isalpha(int c)
 {
-	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
-	{
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
 		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+	return (0);
 }
 
 /*
