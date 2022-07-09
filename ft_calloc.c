b8669f9d821c1f5c61dddc4a235bf4e66a545e46
chanma <yunosuke.homma@icloud.com>
2022-07-10 01:30:53 +0900

    delete include

--- ft_calloc.c
+++ ft_calloc.c
@@ -11,27 +11,43 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 void	*ft_calloc(size_t n, size_t size)
 {
 	void	*rtn_box;
 
-	if (n >= SIZE_MAX / size)
-		return (NULL);
 	if (n == 0 || size == 0)
 	{
 		rtn_box = (void *) malloc(1);
 		if (rtn_box == NULL)
+		{
+			printf("aaa\n");
 			return (NULL);
+		}
 		ft_memset(rtn_box, 0, 1);
 		return (rtn_box);
 	}
+	if (n >= SIZE_MAX / size)
+		return (NULL);
 	rtn_box = (void *) malloc(n * size);
 	if (rtn_box == NULL)
 		return (NULL);
 	ft_memset(rtn_box, 0, n * size);
 	return (rtn_box);
 }
+
+/*#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	char *str;
+
+	str = ft_calloc(0, 0);
+	ft_memset(str, 3, ft_strlen(str));
+	printf("%s\n", str);
+}*/
 
 /*
 void	*ft_calloc(size_t n, size_t size)
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -11,7 +11,6 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 static char	*sp_substr(char const *str, size_t start, size_t size)
 {
