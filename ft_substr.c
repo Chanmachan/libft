26118a9146a12499b0f8291db31a8ab2ecb579dd
chanma <yunosuke.homma@icloud.com>
2022-07-11 02:05:25 +0900

    make it short

--- ft_substr.c
+++ ft_substr.c
@@ -15,25 +15,20 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	size_t	i;
-	char	*new_s;
 	char	*rtn_str;
 	size_t	len_s;
 
 	i = 0;
-	new_s = (char *) s;
 	if (s == NULL)
 		return (NULL);
 	len_s = ft_strlen(s);
 	if (len_s < start)
-	{
-		rtn_str = ft_strdup("\0");
-		return (rtn_str);
-	}
+		return (ft_strdup("\0"));
 	rtn_str = (char *) malloc (sizeof(char) * (ft_strlen(s) - start + 1));
 	if (rtn_str == NULL)
 		return (NULL);
-	while (i < len && new_s[start] != '\0')
-		rtn_str[i++] = new_s[start++];
+	while (i < len && s[start] != '\0')
+		rtn_str[i++] = s[start++];
 	rtn_str[i] = '\0';
 	return (rtn_str);
 }
