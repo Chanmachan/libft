2dd6d49e24c9a4870df0576572a2545f568f34ba
chanma <yunosuke.homma@icloud.com>
2022-07-11 02:17:41 +0900

    add strlcpy

--- ft_substr.c
+++ ft_substr.c
@@ -14,22 +14,18 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	size_t	i;
 	char	*rtn_str;
 	size_t	len_s;
 
-	i = 0;
 	if (s == NULL)
 		return (NULL);
 	len_s = ft_strlen(s);
 	if (len_s < start)
 		return (ft_strdup("\0"));
-	rtn_str = (char *) malloc (sizeof(char) * (ft_strlen(s) - start + 1));
+	rtn_str = (char *) malloc (sizeof(char) * (len_s - start + 1));
 	if (rtn_str == NULL)
 		return (NULL);
-	while (i < len && s[start] != '\0')
-		rtn_str[i++] = s[start++];
-	rtn_str[i] = '\0';
+	ft_strlcpy(rtn_str, &s[start], len + 1);
 	return (rtn_str);
 }
 
