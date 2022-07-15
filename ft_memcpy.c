f1bcec8bf226efd5ae7332e60a69b70119c3cbcd
chanma <yunosuke.homma@icloud.com>
2022-07-16 01:11:17 +0900

    mimic original

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -17,6 +17,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	size_t	i;
 
 	i = 0;
+	if (dst == NULL && src == NULL)
+		return (NULL);
 	while (i < n)
 	{
 		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
@@ -25,3 +27,19 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	return (dst);
 }
 //still copy if '\0'
+//if both args == NULL return (NULL)
+
+/*#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char *dest = NULL;
+	char *src = "abc";
+	ft_memcpy(dest, src, 4);
+	printf("%s\n", dest);
+	char *dest2 = "abc";
+	char *src2 = NULL;
+	memcpy(dest2, src2, 4);
+	printf("%s\n", dest2);
+}*/
