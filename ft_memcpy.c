63d83f0bc5b2091e0aa6067e8257d2ef2a95db71
chanma <yunosuke.homma@icloud.com>
2022-07-18 01:05:58 +0900

    change first if

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -17,8 +17,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	size_t	i;
 
 	i = 0;
-	if (dst == NULL && src == NULL)
-		return (NULL);
+	if (dst == src || n == 0)
+		return (dst);
 	while (i < n)
 	{
 		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
