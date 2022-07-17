c0810e81f3e6db9f88158c1f37c658134b87f3e0
chanma <yunosuke.homma@icloud.com>
2022-07-18 02:50:38 +0900

    int -> long long

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -14,24 +14,21 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	if (n == -2147483648)
+	long long	long_n;
+
+	long_n = n;
+	if (long_n < 0)
 	{
 		ft_putchar_fd('-', fd);
-		ft_putchar_fd('2', fd);
-		n = 147483648;
+		long_n *= -1;
 	}
-	if (n < 0)
+	if (long_n >= 10)
 	{
-		ft_putchar_fd('-', fd);
-		n *= -1;
-	}
-	if (n >= 10)
-	{
-		ft_putnbr_fd(n / 10, fd);
-		ft_putnbr_fd(n % 10, fd);
+		ft_putnbr_fd(long_n / 10, fd);
+		ft_putnbr_fd(long_n % 10, fd);
 	}
 	else
-		ft_putchar_fd(n + 48, fd);
+		ft_putchar_fd(long_n + '0', fd);
 }
 
 /*int main(void)
